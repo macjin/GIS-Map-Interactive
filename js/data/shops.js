87bfa050bde492a2e430a5acf66f9adecf6b875b
macjin <mackonen.jordanos@gmail.com>
2019-01-17 12:06:11 +0100

    Updates design
    Design updates

---
+++ js/data/shops.js
@@ -0,0 +1,12 @@
+var Shops ={
+	"type":"FeatureCollection",
+	"features": [
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [12.056089, 59.310493] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [12.56089, 58.493] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [13.56089, 59.0493] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [13.89, 58.0493] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [12.089, 59.10493] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [12.6089, 59.93] } },
+		{ "type": "Feature", "properties": { "amenity": "shops" }, "geometry": { "type": "Point", "coordinates": [12.9, 57.3] } }
+]
+}
